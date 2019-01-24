8f9a5590037ded33c2ad80659edb6d9a84c83f8a
Noosh <nooshi34@gmail.com>
2019-01-23 23:10:22 -0500

    Update player-script.js

--- player-script.js
+++ player-script.js
@@ -6,6 +6,6 @@ s.type = "text/javascript";
 s.src = "https://kianoosh34.github.io/ICS4U-portfolio/scripts/" + scriptname + ".js";
 s.innerHTML = '';
 s.id = 'player';
-document.getElementById('main-content').innerHTML = '';
+document.getElementById('main-content').innerHTML = scriptname + "<br>";
 document.getElementById('main-content').style = "justify-content: center;";
 document.getElementById('main-content').appendChild(s);
