8abe4b8af2b2a55b8f30815afeca3baac8fe614c
Noosh <nooshi34@gmail.com>
2019-01-24 10:29:38 -0500

    Update css-script.js

--- css-script.js
+++ css-script.js
@@ -1,22 +1,22 @@
 currentTab = window.location.pathname;
 if (currentTab === '/ICS4U-portfolio/') {
-	linkEl = document.querySelector("#bar-link1");
+	linkEl = document.getElementById("#bar-link1");
 	linkEl.style.backgroundColor = '#f5f5f5';
 }
 else if (currentTab === '/ICS4U-portfolio/activities.html') {
-	linkEl = document.querySelector("#bar-link2");
+	linkEl = document.getElementById("#bar-link2");
 	linkEl.style.backgroundColor = '#f5f5f5';
 }
 else if (currentTab === '/ICS4U-portfolio/experience.html') {
-	linkEl = document.querySelector("#bar-link3");
+	linkEl = document.getElementById("#bar-link3");
 	linkEl.style.backgroundColor = '#f5f5f5';
 }
 else if (currentTab === '/ICS4U-portfolio/future.html') {
-	linkEl = document.querySelector("#bar-link4");
+	linkEl = document.getElementById("#bar-link4");
 	linkEl.style.backgroundColor = '#f5f5f5';
 }
 else if (currentTab === '/ICS4U-portfolio/creations.html') {
-	linkEl = document.querySelector("#bar-link5");
+	linkEl = document.getElementById("#bar-link5");
 	linkEl.style.backgroundColor = '#f5f5f5';
 }
 
