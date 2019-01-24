4067edd7a101654759f58ac1f614d059bc02ef98
Noosh <nooshi34@gmail.com>
2019-01-23 23:03:55 -0500

    Update player-script.js

--- player-script.js
+++ player-script.js
@@ -3,7 +3,7 @@ scriptname = params.get('script')
 
 var s = document.createElement("script");
 s.type = "text/javascript";
-s.src = "https://kianoosh34.github.io/ICS4U-portfolio/" + scriptname + ".js";
+s.src = "https://kianoosh34.github.io/ICS4U-portfolio/scripts/" + scriptname + ".js";
 s.innerHTML = '';
 s.id = 'player';
 document.getElementById('main-content').innerHTML = '';
