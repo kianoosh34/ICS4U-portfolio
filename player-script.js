cb567d2115f17c8082e03029e8ea8e801fe47601
Noosh <nooshi34@gmail.com>
2019-01-24 00:12:33 -0500

    Update player-script.js

--- player-script.js
+++ player-script.js
@@ -1,6 +1,13 @@
 params = new URLSearchParams(window.location.search)
 scriptname = params.get('script')
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 var s = document.createElement("script");
 s.type = "text/javascript";
 s.src = "https://kianoosh34.github.io/ICS4U-portfolio/scripts/" + scriptname + ".js";
