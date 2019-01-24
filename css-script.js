715f8b069d0656653c30b34d9b1f099c0df41d67
Noosh <nooshi34@gmail.com>
2019-01-24 13:21:03 -0500

    Update css-script.js

--- css-script.js
+++ css-script.js
@@ -1,22 +1,23 @@
 currentTab = window.location.pathname;
+var mygrey = 'rgb(230,230,230)';
 if (currentTab === '/ICS4U-portfolio/') {
 	linkEl = document.getElementById("bar-link1");
-	linkEl.style.backgroundColor = '#e8e8e8';
+	linkEl.style.backgroundColor = mygrey;
 }
 else if (currentTab === '/ICS4U-portfolio/activities.html') {
 	linkEl = document.getElementById("bar-link2");
-	linkEl.style.backgroundColor = '#e8e8e8';
+	linkEl.style.backgroundColor = mygrey;
 }
 else if (currentTab === '/ICS4U-portfolio/experience.html') {
 	linkEl = document.getElementById("bar-link3");
-	linkEl.style.backgroundColor = '#e8e8e8';
+	linkEl.style.backgroundColor = mygrey;
 }
 else if (currentTab === '/ICS4U-portfolio/future.html') {
 	linkEl = document.getElementById("bar-link4");
-	linkEl.style.backgroundColor = '#e8e8e8';
+	linkEl.style.backgroundColor = mygrey;
 }
 else if (currentTab === '/ICS4U-portfolio/creations.html') {
 	linkEl = document.getElementById("bar-link5");
-	linkEl.style.backgroundColor = '#e8e8e8';
+	linkEl.style.backgroundColor = mygrey;
 }
 
