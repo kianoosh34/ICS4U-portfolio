3a4450db9544116ee586b2dbba1ea0bf0cbe2ea5
Noosh <nooshi34@gmail.com>
2019-01-23 20:44:40 -0500

    Update css-script.js

--- css-script.js
+++ css-script.js
@@ -1,18 +1,17 @@
 currentTab = window.location.pathname;
-linkEl = document.querySelector("#bar-link1");
 if (currentTab === '/ICS4U-portfolio/') {
 	linkEl = document.querySelector("#bar-link1");
 }
-else if (currentTab === '') {
+else if (currentTab === '/ICS4U-portfolio/activities.html') {
 	linkEl = document.querySelector("#bar-link2");
 }
-else if (currentTab === '') {
+else if (currentTab === '/ICS4U-portfolio/experience.html') {
 	linkEl = document.querySelector("#bar-link3");
 }
-else if (currentTab === '') {
+else if (currentTab === '/ICS4U-portfolio/future.html') {
 	linkEl = document.querySelector("#bar-link4");
 }
-else if (currentTab === '') {
+else if (currentTab === '/ICS4U-portfolio/creations.html') {
 	linkEl = document.querySelector("#bar-link5");
 }
 linkEl.style.backgroundColor = '#f5f5f5';
